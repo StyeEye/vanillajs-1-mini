d4f378bf3f8cfad9ff3b65d6370742ddb62f8aa5
StyeEye <ColonialBuff@protonmail.com>
2019-03-13 13:33:10 -0600

    Step 2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let count = 0;
+
+function increase() {
+    count++;
+    console.log(`increased: ${count}`);
+}
+
+function decrease() {
+    count--;
+    console.log(`decreased: ${count}`);
+}
+
+function reset() {
+    count = 0;
+    console.log(`reset: ${count}`);
+}
 console.log(`hello world`);
